e4059e1627798259ae4d4453a91c056d99738669
Omkar Hatankar <hatankaro@gmail.com>
2021-08-02 00:06:33 +0530

    heroku updates - 7

--- gear_shop/settings.py
+++ gear_shop/settings.py
@@ -51,6 +51,7 @@ ALLOWED_HOSTS = ['ggearshop.herokuapp.com', '127.0.0.1',]
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -63,8 +64,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
